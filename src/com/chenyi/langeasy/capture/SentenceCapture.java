a2a95150b2ecc7b3554f708ec2b5c150f068b790
lyz19890927 <lyz19890927@163.com>
2016-09-01 11:44:46 +0800

    add more sentences

--- src/com/chenyi/langeasy/capture/SentenceCapture.java
+++ src/com/chenyi/langeasy/capture/SentenceCapture.java
@@ -1,13 +1,13 @@
 package com.chenyi.langeasy.capture;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,69 +17,113 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SentenceCapture {
-	public static void main(String[] args) throws IOException, JSONException, SQLException {
-		String courseid = "20120727135500100000036";
-		String url = "http://langeasy.com.cn/m/player?f=" + courseid;
-		// Document doc = Jsoup.connect(url).get();
+	private static Connection conn;
 
-		// File courseFile = new File("E:\\sentence.html");
-		// String sResult = IOUtils.toString(new FileInputStream(courseFile), "utf-8");
-		// Document doc = Jsoup.parse(sResult);
-		// Elements liArr = doc.select("#lrcContent li");
-
-		Connection conn = CaptureUtil.getConnection();
+	public static void main(String[] args) throws Exception {
+		conn = CaptureUtil.getConnection();
 		listCourse(conn);
 		CaptureUtil.closeConnection(conn);
 	}
 
-	private static List<Map<String, String>> listCourse(Connection conn)
-			throws JSONException, SQLException, FileNotFoundException, IOException {
-		String sql = "SELECT c.courseid FROM course c left join sentence s on s.courseid = c.courseid "
-				+ "where s.courseid is null group by c.courseid ";
+	private static List<Map<String, String>> listCourse(Connection conn) throws Exception {
+		String sql = "SELECT c.courseid FROM book b inner join course c on c.bookid = b.bookid left join sentence s on s.courseid = c.courseid "
+				+ "where b.bookname like '%老友记%' and s.courseid is null group by c.courseid ";
 		Statement st = conn.createStatement();
 		ResultSet rs = st.executeQuery(sql);
+
+		List<String> courseidLst = new ArrayList<>();
 		while (rs.next()) {
 			String courseid = rs.getString("courseid");
+			courseidLst.add(courseid);
 			// courseid = "20120727135500100000028";
 
-			String url = "http://langeasy.com.cn/m/player?f=" + courseid;
-			Document doc = CaptureUtil.timeoutRequest(url);
-
-			// File courseFile = new File("E:\\sentence.html");
-			// String sResult = IOUtils.toString(new FileInputStream(courseFile), "utf-8");
-			// Document doc = Jsoup.parse(sResult);
-
-			Elements liArr = doc.select("#lrcContent li");
-
-			insertSentence(conn, courseid, liArr);
-			System.out.println(courseid);
-
-			// break;
 		}
 		rs.close();
 		st.close();
 
+		int count = 0;
+		for (String courseid : courseidLst) {
+			count++;
+			System.err.println("find seq : " + count);
+			parseCourse(courseid);
+			// break;
+		}
+
 		return null;
 	}
 
-	private static Integer insertSentence(Connection conn, String courseid, Elements liArr)
-			throws JSONException, SQLException {
-		String insertSql = "INSERT INTO sentence (courseid, type, dataindex, encryptstarttime, text) VALUES (?, ?, ?, ?, ?)";
-		PreparedStatement insPs = conn.prepareStatement(insertSql);
+	private static void parseCourse(String courseid) throws Exception {
+		String url = "http://langeasy.com.cn/m/player?f=" + courseid;
+		Document doc = CaptureUtil.timeoutRequest(url);
+
+		// File courseFile = new File("E:\\sentence.html");
+		// String sResult = IOUtils.toString(new FileInputStream(courseFile), "utf-8");
+		// Document doc = Jsoup.parse(sResult);
+
+		Elements liArr = doc.select("#lrcContent li");
+
+		List<Map<String, Object>> sentenceLst = new ArrayList<>();
 		for (Element li : liArr) {
 			String type = li.attr("class");
 			String dataIndex = li.attr("data-index");
 			String encryptstarttime = li.attr("id");
+			int decodestarttime = 0;
 			if ("".equals(encryptstarttime)) {
 				continue;
+			} else {
+				decodestarttime = CaptureUtil.decodeTime(encryptstarttime);
 			}
 			String text = li.text();
 
+			Map<String, Object> map = new HashMap<>();
+			map.put("dataIndex", Integer.parseInt(dataIndex));
+			map.put("type", type);
+			map.put("text", text);
+			// System.out.println(text);
+			map.put("decodestarttime", decodestarttime);
+			sentenceLst.add(map);
+		}
+		// calculate end time
+		for (int i = 0; i < sentenceLst.size(); i++) {
+			Map<String, Object> sentence = sentenceLst.get(i);
+			String type = (String) sentence.get("type");
+			Integer dataIndex = (Integer) sentence.get("dataIndex");
+			Integer endTime = 0;
+			if (i + 2 <= sentenceLst.size() - 1) {
+				Map<String, Object> nextSentence = sentenceLst.get(i + 2);
+
+				Integer nextIndex = (Integer) nextSentence.get("dataIndex");
+				String nextType = (String) sentence.get("type");
+				if (dataIndex + 1 != nextIndex || !nextType.equals(type)) {
+					throw new Exception("find endtime error, text is :" + sentence.get("text"));
+				}
+				endTime = (Integer) nextSentence.get("decodestarttime");
+			}
+			sentence.put("endtime", endTime);
+		}
+		System.out.println(courseid + ", size: " + sentenceLst.size());
+		insertSentence(courseid, sentenceLst);
+		// System.out.println(sentenceLst);
+
+	}
+
+	private static Integer insertSentence(String courseid, List<Map<String, Object>> sentenceLst)
+			throws JSONException, SQLException {
+		String insertSql = "INSERT INTO sentence (courseid, type, dataindex, text, decodestarttime, endtime) VALUES (?, ?, ?, ?, ?, ?)";
+		PreparedStatement insPs = conn.prepareStatement(insertSql);
+		for (Map<String, Object> sentence : sentenceLst) {
+			String type = (String) sentence.get("type");
+			Integer dataIndex = (Integer) sentence.get("dataIndex");
+			Integer decodestarttime = (Integer) sentence.get("decodestarttime");
+			Integer endtime = (Integer) sentence.get("endtime");
+			String text = (String) sentence.get("text");
+
 			insPs.setString(1, courseid);
 			insPs.setString(2, type);
-			insPs.setInt(3, Integer.parseInt(dataIndex));
-			insPs.setString(4, encryptstarttime);
-			insPs.setString(5, text);
+			insPs.setInt(3, dataIndex);
+			insPs.setString(4, text);
+			insPs.setInt(5, decodestarttime);
+			insPs.setInt(6, endtime);
 			insPs.addBatch();
 		}
 		try {
