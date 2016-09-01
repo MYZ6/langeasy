a62ab35bf559c59d5155291a52efaa6b132f798c
lyz19890927 <lyz19890927@163.com>
2016-09-01 11:44:46 +0800

    add more books and courses

--- src/com/chenyi/langeasy/capture/BookCapture.java
+++ src/com/chenyi/langeasy/capture/BookCapture.java
@@ -1,94 +1,88 @@
 package com.chenyi.langeasy.capture;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringEscapeUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class BookCapture {
 
+	private static CloseableHttpClient httpclient;
+
 	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		List<JSONObject> allCarLst = new ArrayList<>();
-		// subidArr = new String[] { "2124" };
-		int count = 0;
-		// for (String subid : subidArr) {
-		System.out.println(allCarLst.size());
+		httpclient = HttpClients.createDefault();
 
-		JSONObject book = getBookInfo(httpclient, "1688236492");
+		// JSONObject book = getBookInfo("1688236492");
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
+		Connection conn = CaptureUtil.getConnection();
 		// insertBook(conn, book);
+		listBook(conn);
+
 		conn.close();
+		httpclient.close();
 	}
 
-	public static JSONObject getBookInfo(CloseableHttpClient httpclient, String bookid)
-			throws ClientProtocolException, IOException {
-		String requestUrl = "http://langeasy.com.cn/getBookInfo.action?bookid=" + bookid
-				+ "&courpage=1&courrows=1666&pubstate=1";
-		HttpPost httppost = new HttpPost(requestUrl);
+	private static void listBook(Connection conn) throws SQLException, FileNotFoundException, IOException {
+		// String requestUrl =
+		// "http://langeasy.com.cn/newlisten/bookResult.action?bookcells=86&bookpage=1&lrctype=&sort=1&type=%E7%BE%8E%E5%89%A7%26%E8%8B%B1%E5%89%A7";
+		// HttpPost httppost = new HttpPost(requestUrl);
+		//
+		// CloseableHttpResponse response = httpclient.execute(httppost);
+		// // Get hold of the response entity
+		// HttpEntity entity = response.getEntity();
+		//
+		// byte[] result = null;
+		// // If the response does not enclose an entity, there is no need
+		// // to bother about connection release
+		// if (entity != null) {
+		// InputStream instream = entity.getContent();
+		// result = IOUtils.toByteArray(instream);
+		// instream.close();
+		// }
+		// response.close();
 
-		CloseableHttpResponse response = httpclient.execute(httppost);
-		// Get hold of the response entity
-		HttpEntity entity = response.getEntity();
+		File bookFile = new File("E:\\booklist.json");
+		String sResult = IOUtils.toString(new FileInputStream(bookFile), "utf-8");
 
-		byte[] result = null;
-		// If the response does not enclose an entity, there is no need
-		// to bother about connection release
-		if (entity != null) {
-			InputStream instream = entity.getContent();
-			result = IOUtils.toByteArray(instream);
-			instream.close();
-		}
-		response.close();
-
-		String sResult = new String(result, "utf-8");
+		// String sResult = new String(result, "utf-8");
 		sResult = StringEscapeUtils.unescapeJava(sResult);
-		System.out.println(sResult);
-		JSONObject book = null;
+		// System.out.println(sResult);
+		JSONArray booklist = null;
 		try {
-			book = new JSONObject(sResult);
+			JSONObject json = new JSONObject(sResult);
+			booklist = json.getJSONArray("booklist");
+			System.out.println(booklist.length());
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(sResult);
 		}
-		return book;
+
+		insertBook(conn, booklist);
 	}
 
-	// JDBC driver name and database URL
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/langeasy";
-
-	// Database credentials
-	static final String USER = "root";
-	static final String PASS = "";
-
-	private static Integer insertBook(Connection conn, JSONObject book) throws JSONException, SQLException {
+	private static Integer insertBook(Connection conn, JSONArray booklist) throws JSONException, SQLException {
 		String insertSql = "INSERT INTO book (bookid, bookname, booktype, detail, coverpath) VALUES (?, ?, ?, ?, ?)";
 		PreparedStatement insPs = conn.prepareStatement(insertSql);
-		insPs.setString(1, book.getString("bookid"));
-		insPs.setString(2, book.getString("bookname"));
-		insPs.setString(3, book.getString("booktype"));
-		insPs.setString(4, book.getString("detail"));
-		insPs.setString(5, book.getString("coverpath"));
-		insPs.addBatch();
+		for (int i = 0; i < booklist.length(); i++) {
+			JSONObject book = booklist.getJSONObject(i);
+			insPs.setString(1, book.getString("bookid"));
+			insPs.setString(2, book.getString("bookname"));
+			insPs.setString(3, "美剧&英剧");
+			insPs.setString(4, "");
+			insPs.setString(5, book.getString("imagepath"));
+			insPs.addBatch();
+		}
 
 		insPs.executeBatch();
 		insPs.clearBatch();
--- src/com/chenyi/langeasy/capture/CourseCapture.java
+++ src/com/chenyi/langeasy/capture/CourseCapture.java
@@ -2,17 +2,23 @@ package com.chenyi.langeasy.capture;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONArray;
@@ -20,27 +26,45 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CourseCapture {
+	private static CloseableHttpClient httpclient;
+	private static Connection conn;
 
 	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		List<JSONObject> allCarLst = new ArrayList<>();
-		// subidArr = new String[] { "2124" };
-		int count = 0;
-		// for (String subid : subidArr) {
-		System.out.println(allCarLst.size());
+		httpclient = HttpClients.createDefault();
 
-		JSONObject book = getBookInfo(httpclient, "1688236492");
+		conn = CaptureUtil.getConnection();
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		listBook();
 
-		// insertBook(conn, book);
-		insertCourse(conn, book.getJSONArray("courlist"));
+		// getBookInfo("1063472540");
 		conn.close();
+
+		httpclient.close();
 	}
 
-	public static JSONObject getBookInfo(CloseableHttpClient httpclient, String bookid)
-			throws ClientProtocolException, IOException {
+	private static void listBook() throws SQLException, FileNotFoundException, IOException {
+		String sql = "select * from book where bookid not in (select bookid from course group by bookid)";
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		List<String> booklst = new ArrayList<>();
+		while (rs.next()) {
+			String bookid = rs.getString("bookid");
+			booklst.add(bookid);
+		}
+		rs.close();
+		st.close();
+
+		int count = 0;
+		for (String bookid : booklst) {
+			count++;
+			System.err.println("find seq : " + count);
+
+			getBookInfo(bookid);
+		}
+	}
+
+	public static JSONObject getBookInfo2(String bookid)
+			throws ClientProtocolException, IOException, JSONException, SQLException {
 		File courseFile = new File("E:\\course.json");
 		String sResult = IOUtils.toString(new FileInputStream(courseFile), "utf-8");
 		sResult = StringEscapeUtils.unescapeJava(sResult);
@@ -52,16 +76,42 @@ public class CourseCapture {
 			e.printStackTrace();
 			System.out.println(sResult);
 		}
+
 		return book;
 	}
 
-	// JDBC driver name and database URL
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/langeasy";
+	public static void getBookInfo(String bookid)
+			throws ClientProtocolException, IOException, JSONException, SQLException {
+		String requestUrl = "http://langeasy.com.cn/getBookInfo.action?bookid=" + bookid
+				+ "&courpage=1&courrows=1666&pubstate=1";
+		HttpPost httppost = new HttpPost(requestUrl);
 
-	// Database credentials
-	static final String USER = "root";
-	static final String PASS = "";
+		CloseableHttpResponse response = httpclient.execute(httppost);
+		// Get hold of the response entity
+		HttpEntity entity = response.getEntity();
+
+		byte[] result = null;
+		// If the response does not enclose an entity, there is no need
+		// to bother about connection release
+		if (entity != null) {
+			InputStream instream = entity.getContent();
+			result = IOUtils.toByteArray(instream);
+			instream.close();
+		}
+		response.close();
+
+		String sResult = new String(result, "utf-8");
+		// sResult = StringEscapeUtils.unescapeJava(sResult);
+		System.out.println(sResult);
+		JSONObject book = null;
+		try {
+			book = new JSONObject(sResult);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(sResult);
+		}
+		insertCourse(book.getJSONArray("courlist"));
+	}
 
 	private static Integer insertBook(Connection conn, JSONObject book) throws JSONException, SQLException {
 		String insertSql = "INSERT INTO book (bookid, bookname, booktype, detail, coverpath) VALUES (?, ?, ?, ?, ?)";
@@ -80,7 +130,7 @@ public class CourseCapture {
 		return 0;
 	}
 
-	private static Integer insertCourse(Connection conn, JSONArray courseLst) throws JSONException, SQLException {
+	private static Integer insertCourse(JSONArray courseLst) throws JSONException, SQLException {
 		String insertSql = "INSERT INTO course (courseid, bookid, name, mp3path) VALUES(?, ?, ?, ?)";
 		PreparedStatement insPs = conn.prepareStatement(insertSql);
 		for (int i = 0; i < courseLst.length(); i++) {
