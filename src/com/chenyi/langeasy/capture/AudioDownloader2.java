d1c17967d3f902d7218fa91f6301cd56ca283f49
lyz19890927 <lyz19890927@163.com>
2016-09-14 10:55:56 +0800

    handle mp3 download exceptions

---
+++ src/com/chenyi/langeasy/capture/AudioDownloader2.java
@@ -0,0 +1,146 @@
+package com.chenyi.langeasy.capture;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class AudioDownloader2 {
+	private static CloseableHttpClient httpclient;
+
+	private static List<Map<String, String>> downloadLst;
+
+	public static void main(String[] args) throws FileNotFoundException, SQLException, IOException {
+		httpclient = HttpClients.createDefault();
+		System.out.println("start time is : " + new Date());
+		listCourse();
+		// httpclient.close();
+	}
+
+	private static void listCourse() throws SQLException, FileNotFoundException, IOException {
+		String[] fileArr = new String[] { "ListenData/269296427/1684575709.mp3", "ListenData/1404568722/1415012900.mp3",
+				"ListenData/977915444/2054856991.mp3", "ListenData/538418611/216561888.mp3",
+				"ListenData/100279329/1230981771.mp3", "ListenData/349559752/1919249301.mp3",
+				"ListenData/538418611/1235380786.mp3", "ListenData/1694965458/1154810428.mp3",
+				"ListenData/349559752/2036962820.mp3", "ListenData/1694965458/376064581.mp3",
+				"ListenData/538418611/1600531794.mp3", "ListenData/1983179718/680991519.mp3",
+				"ListenData/134132630/592709876.mp3", "ListenData/915900259/1952231911.mp3",
+				"ListenData/270963162/1379157890.mp3" };
+		int count = 0;
+		downloadLst = new ArrayList<>();
+		for (String mp3path : fileArr) {
+			count++;
+			System.out.println("find seq : " + count);
+			String filepath = "e:/langeasy/" + mp3path;// "ListenData/1688236492/2089837271.mp3";
+			File saveFile = new File(filepath);
+			if (saveFile.exists()) {
+				// continue;
+			}
+			String dirpath = saveFile.getParent();
+			File dir = new File(dirpath);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			System.out.println(dirpath);
+
+			Map<String, String> map = new HashMap<>();
+			map.put("mp3path", mp3path);
+			map.put("saveFilePath", filepath);
+			downloadLst.add(map);
+			// downloadMp3(mp3path, saveFile);
+		}
+		int total = downloadLst.size();// about 1800
+		System.out.println(total);
+		if (total > -1) {
+			// return;
+		}
+
+		AudioDownloader2 downloader = new AudioDownloader2();
+		for (int i = 0; i < 15; i++) {
+			Job job = downloader.new Job(i);
+			job.start();
+		}
+
+	}
+
+	class Job implements Runnable {
+		private Thread t;
+		private int jobIndex;
+
+		Job(int jobIndex) {
+			this.jobIndex = jobIndex;
+			System.out.println("Creating job " + jobIndex);
+		}
+
+		public void run() {
+			int step = 1;
+			int start = jobIndex * step;
+			System.out.println(start + "\t" + (start + step));
+			List<Map<String, String>> subLst = downloadLst.subList(start, start + step);
+			int count = 0;
+			for (Map<String, String> map : subLst) {
+				count++;
+				System.err.println("job" + jobIndex + " download seq : " + count);
+				try {
+					downloadMp3(map.get("mp3path"), map.get("saveFilePath"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public void start() {
+			System.out.println("Starting job " + jobIndex);
+			if (t == null) {
+				t = new Thread(this, "job" + jobIndex);
+				t.start();
+			}
+		}
+
+	}
+
+	private static void downloadMp3(String mp3path, String saveFilePath) throws ClientProtocolException, IOException {
+		System.out.println(saveFilePath + ", start time is : " + new Date());
+		long start = System.currentTimeMillis();
+
+		File saveFile = new File(saveFilePath);
+
+		String url = "http://langeasy.com.cn/" + mp3path;// "ListenData/1688236492/2089837271.mp3";
+		HttpGet httpget = new HttpGet(url);
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		if (entity != null) {
+			long startTime = System.currentTimeMillis();
+			System.out.println(mp3path + " download begin");
+			InputStream inputStream = entity.getContent();
+			OutputStream outputStream = new FileOutputStream(saveFile);
+			IOUtils.copy(inputStream, outputStream);
+			outputStream.close();
+			System.out.println(mp3path + " download success");
+			long endTime = System.currentTimeMillis();
+			System.out.println("download time elapsed: " + (endTime - startTime));
+		}
+		httpclient.close();
+		long end = System.currentTimeMillis();
+		System.out.println(saveFilePath + ", consuming seconds : " + (end - start));
+
+	}
+}
---
+++ src/com/chenyi/langeasy/capture/AudioErrorHandler.java
@@ -0,0 +1,69 @@
+package com.chenyi.langeasy.capture;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AudioErrorHandler {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("start time is : " + new Date());
+		test();
+		// testCourse();
+	}
+
+	public static void test() throws IOException {
+		File dir = new File("E:/langeasy/sentence");
+		int count = 0;
+		List<String> sentenceLst = new ArrayList<>();
+		for (File file : dir.listFiles()) {
+			count++;
+			// if (file.getName().indexOf("412125") > -1) {
+			// System.out.println(file.getName() + "\t" + file.length());
+			// }
+			if (count % 500 != 0) {
+				// continue;
+			}
+			long length = file.length();
+			if (length / 1000 < 20) {
+				String filename = file.getName();
+				System.out.println(filename + "\t" + length);
+				sentenceLst.add(filename.substring(0, filename.length() - 4));
+			}
+		}
+		System.out.println(sentenceLst);
+		System.out.println(sentenceLst.size());
+
+	}
+
+	public static void testCourse() throws IOException {
+		int count = 0;
+		String[] fileArr = new String[] { "ListenData/538418611/88595807.mp3", "ListenData/538418611/1789362336.mp3",
+				"ListenData/269296427/1684575709.mp3", "ListenData/1404568722/1415012900.mp3",
+				"ListenData/977915444/2054856991.mp3", "ListenData/538418611/216561888.mp3",
+				"ListenData/100279329/1230981771.mp3", "ListenData/538418611/198037860.mp3",
+				"ListenData/100279329/1423463868.mp3", "ListenData/351571393/1388937632.mp3",
+				"ListenData/1472489706/901528436.mp3", "ListenData/349559752/1864016057.mp3",
+				"ListenData/349559752/1919249301.mp3", "ListenData/1239256970/2012163489.mp3",
+				"ListenData/538418611/1235380786.mp3", "ListenData/1694965458/1154810428.mp3",
+				"ListenData/349559752/2036962820.mp3", "ListenData/1694965458/376064581.mp3",
+				"ListenData/279257204/1423498389.mp3", "ListenData/538418611/1600531794.mp3",
+				"ListenData/1983179718/680991519.mp3", "ListenData/538418611/608250557.mp3",
+				"ListenData/134132630/592709876.mp3", "ListenData/915900259/1952231911.mp3",
+				"ListenData/270963162/1379157890.mp3", "ListenData/1694965458/1630553259.mp3" };
+		String prefix = "E:/langeasy/";
+		for (String filepath : fileArr) {
+			count++;
+			File file = new File(prefix + filepath);
+			long length = file.length();
+			String filename = file.getName();
+			// if (length / 1000 < 100) {
+			System.out.println(filename + "\t" + length / 1000);
+			System.out.println("\"" + filepath + "\",");
+			// }
+		}
+
+	}
+}
